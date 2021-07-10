e88544f22eb8bcf0fc77ef1151fe48a4bf398fb4
jenaze <37847028+jenaze@users.noreply.github.com>
2021-07-10 06:24:57 +0000

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -19,10 +19,47 @@ mkdir "C:\Users\Public\setup"
 curl -o "C:\Users\Public\setup\winrar.exe" https://www.rarlab.com/rar/winrar-x64-602.exe > out.txt 2>&1
 curl -o "C:\Users\Public\setup\brave.exe" https://brave-browser-downloads.s3.brave.com/latest/brave_installer-x64.exe > out.txt 2>&1
 curl -o "C:\Users\Public\setup\npp.exe" https://raw.githubusercontent.com/jenaze/hrdp/main/Files/npp.7.9.4.Installer.x64.exe > out.txt 2>&1
+curl -o "C:\Users\Public\setup\pnn.zip" https://raw.githubusercontent.com/jenaze/hrdp/main/Files/pnn.zip > out.txt 2>&1
 C:\Users\Public\setup\winrar.exe /S
 C:\Users\Public\setup\brave.exe --install --silent --system-level
 C:\Users\Public\setup\npp.exe /S
+curl -o "C:\Program Files\WinRAR\rarreg.key" https://raw.githubusercontent.com/jenaze/hrdp/main/Files/rarreg.key > out.txt 2>&1
 ftype txtfile="C:\Program Files\Notepad++\notepad++.exe" "%1"
+cd C:\Users\Public\setup\
+jar xf C:\Users\Public\setup\pnn.zip
+
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\nvpn1.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "C:\Users\Public\setup\npnAccountX\NordVpn Checker Account  By X-KILLER.exe" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+
+
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\nvpn2.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "C:\Users\Public\setup\nvpnMonacoa\NordVpn.exe" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\Proxy Generator.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "C:\Users\Public\setup\Pgnarator\Proxy Generator 1.3.6 BETA.exe" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
 
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
