82c956035818e41b8ad3acec6400473b2042e9fa
jenaze <37847028+jenaze@users.noreply.github.com>
2021-08-06 12:51:17 +0430

    address

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -12,8 +12,13 @@ net user administrator @#Hsh123456 /add >nul
 net localgroup administrators administrator /add >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo User: Administrator
+tasklist | find /i "ngrok.exe" >Nul && For /F "Delims=" %%A In ('"curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url"') Do Set "test=%%~A" || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels "
+
+set word=bitspy.ir
+call set str=%%test:tcp.ngrok.io=%word%%%
+call set str=%%str:tcp://=%%
+
+echo sv%str%echo User: Administrator
 echo Pass: @#Hsh123456
 curl -O https://raw.githubusercontent.com/jenaze/hrdp/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 
